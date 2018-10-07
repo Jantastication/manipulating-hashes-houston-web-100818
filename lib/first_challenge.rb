a370ae17e4456d4bae0d268c53e17456df890f1c
Jantastication O <georginaoregx@gmail.com>
2018-10-07 05:17:08 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,23 +16,15 @@ def first_challenge
   }
 
   #your code here
-     def remove_strawberry(contacts)
-       contacts.each do |person, contact_details_hash|
-       contact_details_hash.each do |attribute, data|
-         if attribute == :favorite_ice_cream_flavors
-          data.delete_if{|ice_cream| ice_cream == "strawberry"}
-         end
-       end
+
+    def remove_strawberry(contacts)
+      contacts.each do |person, data|
+        data.each do |attribute, value|
+          if attribute == :favorite_icecream_flavors
+              value.delete_if {|flavor| flavor == "strawberry"}
+          end
+        end
       end
     end
-    #def remove_strawberry(contacts)
-      #contacts.each do |person, data_hash|
-        #data_hash.each do |attribute, value|
-          #if attribute == :favorite_icecream_flavors
-              #value.delete_if {|flavor| flavor == "strawberry"}
-          #end
-        #end
-      #end
-    #end
   contacts
 end
